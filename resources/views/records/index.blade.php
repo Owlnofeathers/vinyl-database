3599d38b4af1514c0bd3bb16984c274eb29ae3a3
Adam Bailey <abailey@mcclainconcepts.com>
2016-05-08 21:19:15 -0700

    table of records in the db

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -4,6 +4,20 @@
 
     <div class="container">
         <h1 class="page-header">Adam's Vinyl</h1>
+
+        <table class="table table-striped">
+            <tr>
+                <td>Artist</td>
+                <td>Album</td>
+            </tr>
+            @foreach( $records as $record )
+                <tr>
+                    <td>{{ $record->artist->name }}</td>
+                    <td>{{ $record->title }}</td>
+                </tr>
+            @endforeach
+        </table>
+
     </div>
 
 @endsection
