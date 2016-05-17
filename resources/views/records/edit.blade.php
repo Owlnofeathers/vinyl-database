c6e2d4f7493e7f46a5e5e3254ad6da250946dd7f
Adam Bailey <abailey@mcclainconcepts.com>
2016-05-16 21:48:27 -0700

    Adds title and artist to edit page

--- resources/views/records/edit.blade.php
+++ resources/views/records/edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class ="container">
+        <h1>Edit Record Contents</h1>
+        <h2 class="page-header">{{ $record->artist->name }} - {{ $record->title }}</h2>
         <form action="#" method="POST">
         <input type="hidden" name="_method" value="PUT">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -12,8 +14,8 @@
 
             @foreach($contents as $key => $value)
                 <div class="form-group">
-                    <label for="{{ $key }}">{{ $key }}</label>
-                    <input type="text" name="contents" class="form-control" placeholder="{{$value}}">
+                    <label for="">{{ $key }}</label>
+                    <input type="text" name="{{ $key }}" class="form-control" placeholder="{{$value}}">
                 </div>
             @endforeach
             <button type="submit" class="btn btn-default">Submit</button><br>
