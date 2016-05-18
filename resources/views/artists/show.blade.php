87d8065ca043957239942c8dcb7b1bd1bf146008
Adam Bailey <abailey@mcclainconcepts.com>
2016-05-17 21:47:53 -0700

    Adds artist show view

---
+++ resources/views/artists/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class ="container">
+        <h1>{{ $record->artist->name }}</h1>
+        <h2 class=page-header">{{ $record->title }}
+            <a href="/record/{{ $record->id }}/edit" type="button" title="Edit {{ $record->title }}">
+                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+            </a>
+        </h2>
+
+        <table class="table table-striped">
+            <tr>
+                @foreach( $contents as $key => $value )
+                    <td><strong>{{ $key }}</strong></td>
+                @endforeach
+            </tr>
+            <tr>
+                @foreach( $contents as $key => $value )
+                    <td>{{ $value }}</td>
+                @endforeach
+            </tr>
+        </table>
+        <a href="/records" class="btn btn-primary btn-lg" role="button">Back to all records</a>
+
+    </div>
+@endsection
