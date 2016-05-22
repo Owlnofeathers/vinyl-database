e5900bcb7784b91bb420c204ba30c7539b59be09
Adam Bailey <abailey@mcclainconcepts.com>
2016-05-21 21:19:06 -0700

    Adds search bar partial

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -5,6 +5,8 @@
     <div class="container">
         <h1 class="page-header">Adam's Vinyl</h1>
 
+        @include('partials.search',['url'=>'records'])
+
             @foreach( $records as $record )
                     <p class="lead">
                         <a href="/record/{{ $record->id }}" title="View {{ $record->title }}"">{{ $record->artist->name }} - {{ $record->title }}</a>
