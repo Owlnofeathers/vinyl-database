403de805ac6aea600975b123461fb7a4e1897832
Adam Bailey <abailey@mcclainconcepts.com>
2016-06-03 21:22:18 -0700

    More HTML fixins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.main')
 
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Adam's Vinyl Database</h1>
-            <p>Site to collect and gather data about the records and the people who make them</p>
-            <p><a class="btn btn-primary btn-lg" href="/records" role="button">Browse Records</a></p>
-        </div>
-    </div>
-
     <!-- Intro Section -->
     <section id="intro" class="intro-section">
         <div class="container">
