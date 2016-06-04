41428bfc0056bc6d10558b1f55eb5fd568d27683
Adam Bailey <abailey@mcclainconcepts.com>
2016-06-03 21:47:57 -0700

    Some adjustments and comment removal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,16 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Adam's Vinyl Collection</title>
 
-    <!-- Bootstrap -->
     <link href="/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ elixir("css/all.css") }}">
-    {{--<link href="css/scrolling-nav.css" rel="stylesheet">--}}
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -27,15 +22,11 @@
 
 @yield('content')
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
+
 <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
 
 <script src="{{ elixir("js/all.js") }}"></script>
-{{--<!-- Scrolling Nav JavaScript -->
-<script src="js/jquery.easing.min.js"></script>
-<script src="js/scrolling-nav.js"></script>--}}
 
 @yield('scripts')
 
