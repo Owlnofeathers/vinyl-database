fd8784c6cb53b6d80ab1d5db278b9b35f4fc21bd
Adam Bailey <abailey@mcclainconcepts.com>
2016-06-08 21:39:52 -0700

    Adds create page for records

---
+++ resources/views/records/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class ="container">
+        <h1 class="page-header">Add Record</h1>
+
+        <div class="col-md-6">
+            <form action="/record" method="POST">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div class="form-group">
+                    <label for="">Artist or Band Name</label>
+                    <select class="form-control" name="artist-name">
+                        @foreach($artists as $artist)
+                            <option value="">{{ $artist->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Catalog Number</label>
+                    <input type="text" name ="catalog-number" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Vinyl Color</label>
+                    <input type="text" name ="vinyl-color" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Pressing Information</label>
+                    <input type="text" name ="pressing-info" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Vinyl Size</label>
+                    <select class="form-control" name="vinyl-size">
+                        @foreach($sizes as $size)
+                            <option value="{{ $size }}">{{ $size }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Genre</label>
+                    <input type="text" name ="genre" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Link A Photo</label>
+                    <input type="text" name ="photo-link" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Condition</label>
+                    <select class="form-control" name="condition">
+                        @foreach($conditions as $condition)
+                            <option value="{{ $condition }}">{{ $condition }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+
+                <button type="submit" class="btn btn-default btn-lg">Submit</button>
+                <a href="/record" class="btn btn-danger btn-lg" role="button">Cancel</a>
+            </form>
+        </div>
+
+    </div>
+@endsection
