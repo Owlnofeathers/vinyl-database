5c2eea698947369fb49b36ee873312e4ea862132
Adam Bailey <abailey@mcclainconcepts.com>
2016-06-29 16:56:13 -0700

    Added artists thru B to the seeder

--- database/seeds/ArtistsTableSeeder.php
+++ database/seeds/ArtistsTableSeeder.php
@@ -52,6 +52,138 @@ class ArtistsTableSeeder extends Seeder
                 'name' => 'Al Hirt',
                 'description' => 'Trumpeter, born 7 November 1922 in New Orleans, Louisiana, died 27 April 1999 in New Orleans, Louisiana.'
             ],
+            [
+                'name' => 'Alice Cooper',
+                'description' => 'The individual, Vince Furnier, who was a member of the band Alice Cooper and who later changed his name to Alice Cooper. '
+            ],
+            [
+                'name' => 'Alkaline Trio',
+                'description' => 'Melodic punk band from the USA, formed by guitar/vocalist Matt Skiba in 1997. The band released most of their early material on Asian Man Records before eventually signing to Vagrant Records.'
+            ],
+            [
+                'name' => 'American Steel',
+                'description' => 'American Punk Rock band formed in 1995 in Oakland, California.'
+            ],
+            [
+                'name' => 'Ancient Warlocks',
+                'description' => 'Ancient Warlocks are a band from Seattle, WA, which has been steadily building its sound and fan base since their first live show in November of 2010.'
+            ],
+            [
+                'name' => 'APMD',
+                'description' => 'Also known as All Pigs Must Die.'
+            ],
+            [
+                'name' => 'As Friends Rust',
+                'description' => 'American punk/hardcore band founded in 1996 in Gainesville, Florida.'
+            ],
+            [
+                'name' => 'ASG',
+                'description' => 'Formed in the North Carolina coastline community of Wrightsville Beach in 2001.'
+            ],
+            [
+                'name' => 'At The Drive In',
+                'description' => 'Combining emotional melodies and an upbeat rhythm moving at an unpredictable rate, At the Drive-In definitely stuck out in their hometown El Paso, TX.'
+            ],
+            [
+                'name' => 'At The Gates',
+                'description' => 'At the Gates was formed in 1990 in Gothenburg, Sweden on the ashes of split-up death/black metal band Grotesque.'
+            ],
+            [
+                'name' => 'Avail',
+                'description' => 'American Punk band from Richmond, Virginia. Formed in 1987, split up in 2008.'
+            ],
+            [
+                'name' => 'B.W. Stevenson',
+                'description' => 'B.W. stands for Buckwheat. Stevenson from Dallas, Texas was born October 5, 1949 and died April 28, 1988 during heart surgery aged 38.'
+            ],
+            [
+                'name' => 'Bad Brains',
+                'description' => 'American Hardcore Punk band formed in Washington, D.C. in 1977, initially as a jazz fusion ensemble called Mind Power.'
+            ],
+            [
+                'name' => 'Bad Chopper',
+                'description' => 'Band formed by CJ Ward (aka C.J. Ramone) after the break-up of Los Gusanos.'
+            ],
+            [
+                'name' => 'Bad Cop/Bad Cop',
+                'description' => 'With catchy hooks, three part harmonies, and a drummer who fires her ‘guns’ harder than most of her male compatriots, Bad Cop/Bad Cop bring to mind the ‘90s heyday of chicks who actually rocked.'
+            ],
+            [
+                'name' => 'Bad Religion',
+                'description' => 'Punk/Rock band formed in 1980 in San Fernando Valley/Los Angeles, CA, US by Greg Graffin (vocals), Brett Gurewitz (guitar), Jay Ziskrout (drums), and Jay Bentley (bass).'
+            ],
+            [
+                'name' => 'Baroness',
+                'description' => 'American Stoner Rock / Metal band from Savannah, Georgia. Baroness formed in mid-2003, founded by former members of the Punk/Metal band Johnny Welfare And The Paychecks.'
+            ],
+            [
+                'name' => 'Battles',
+                'description' => 'American experimental rock group, founded in 2002 in New York City.'
+            ],
+            [
+                'name' => 'Big Business',
+                'description' => 'Big Business started 2004 as a two piece band composed of Jared Warren of Karp and The Tight Bros From Way Back When, and Coady Willis of Murder City Devils.'
+            ],
+            [
+                'name' => 'Bl\'ast',
+                'description' => 'Bl\'ast! is a hardcore band from Santa Cruz, CA, USA who formed in 1983. In 1986, the band released their first album, "The Power Of Expression".'
+            ],
+            [
+                'name' => 'Black Prism',
+                'description' => ''
+            ],
+            [
+                'name' => 'Blink 182',
+                'description' => 'A Southern California pop-punk band. Originally named Blink until the Irish pop band of the same name threatened with legal action unless they changed their name. Thus the addition of the 182. '
+            ],
+            [
+                'name' => 'Bob Dylan',
+                'description' => 'Born: May 24, 1941, Duluth, Minnesota, USA; singer, songwriter, "song and dance man". Inducted into Songwriters Hall of Fame in 1982 and the Rock And Roll Hall of Fame in 1988 (Performer).'
+            ],
+            [
+                'name' => 'Boots Randolph',
+                'description' => 'Boots was an American musician best known for his 1963 saxophone hit, "Yakety Sax" (Benny Hill\'s signature tune).'
+            ],
+            [
+                'name' => 'Born Against',
+                'description' => 'Hardcore punk band formed in New York in 1989. Known for fusing their music with politics and humor. They disbanded in 1993.'
+            ],
+            [
+                'name' => 'Botch',
+                'description' => 'Hardcore/Math-Rock band from Tacoma, Washington. Formed in 1993, Disbanded in 2002.'
+            ],
+            [
+                'name' => 'Bouncing Souls',
+                'description' => 'Melodic "sing along" pop punk band from New Jersey, originally formed in 1987 while the band members were still in high school. In 1993, They formed Chunksaah Records to release their early material.'
+            ],
+            [
+                'name' => 'Boxcar Willie',
+                'description' => 'American country musician, born 1 September 1931 in Red Oak, Texas, USA, died 12 April 1999 in Branson, Missouri, USA.'
+            ],
+            [
+                'name' => 'Boysetsfire',
+                'description' => 'Political Post-Hardcore/Emo-core band from Newark, Delaware, founded in 1994, disbanded in 2006/2007 (announced/farewell show) and reunited in October 2010.'
+            ],
+            [
+                'name' => 'Brujeria',
+                'description' => 'The band name derives from the black magic used by some Mexican drug dealers to inspire fear in religious locals. The publicity surrounding the release of Matando Gueros painted a similar picture, alleging that the band were, in fact, seven psychotic Mexican drug barons, all Satanists, led by vocalist Juan Brujo, and featuring three bassists.'
+            ],
+            [
+                'name' => 'Buddy Rich',
+                'description' => 'American jazz drummer and band leader. Born September 30, 1917 in Brooklyn, New York. Died April 2, 1987, of heart failure following surgery for a malignant brain tumor.'
+            ],
+            [
+                'name' => 'Burl Ives',
+                'description' => 'Born in Hunt City Township, Illinois on June 14, 1909. It has been said he gave his first professional performance at age 4 in 1913, singing "Barbara Allen" at a picnic, which earned him one dollar.'
+            ],
+            [
+                'name' => 'Burt Bacharach',
+                'description' => 'American pianist and composer. Award-winning pianist and composer, he is best known for composing, and often performing, his many pop hits from 1962-70, mainly with lyrics written by Hal David.'
+            ],
+            [
+                'name' => 'BXI',
+                'description' => 'Boris x Ian Astbury collaboration.'
+            ],
 
         ]);
     }
