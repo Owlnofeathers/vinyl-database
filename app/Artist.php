81f7c2380ec34c3e601669531cfd96d98d679de4
Adam Bailey <owlnofeathers@gmail.com>
2016-08-19 21:01:32 -0700

    Adds protected fillable to models

--- app/Artist.php
+++ app/Artist.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {
-
+    protected $fillable = ['name'];
 
     public function records()
     {
--- app/Label.php
+++ app/Label.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Label extends Model
 {
+    protected $fillable = ['name'];
+
     public function records()
     {
         return $this->hasMany('App\Record');
