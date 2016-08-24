f0d90a7fdaf7eda16ca9b4b2ba0624485a08542b
Adam Bailey <owlnofeathers@gmail.com>
2016-08-23 20:51:22 -0700

    Styles on record.show

--- resources/views/records/show.blade.php
+++ resources/views/records/show.blade.php
@@ -21,14 +21,14 @@
                         </h3>
                         <p>This copy of {{ $record->title }} is a {{ $contents['vinyl-color'] }} {{ $contents['vinyl-size'] }}" {{ $record->genre->name }} record,
                             pressed by {{ $record->label->name }}
-                            @if(!empty($contents['catalog-number']))
-                                (catalog number {{ $contents['catalog-number'] }})
+                            @if( !empty($contents['catalog-number']))
+                                <p>The catalog number is: {{ $contents['catalog-number'] }}</p>
                             @endif
                         </p>
 
-                        @if(!empty($contents['pressing-info'] ))
+                        @if( !empty($contents['pressing-info'] ))
                             <ul>
-                                <li>{{ $contents['pressing-info'] }}.</li>
+                                <li class="list-unstyled">{{ $contents['pressing-info'] }}.</li>
                             </ul><br>
                         @endif
 
@@ -41,7 +41,7 @@
                         @endif
 
                         <div class="img-responsive text-center">
-                            <img class="img-circle" src="{{ $faker->imageUrl(300, 300, 'cats') }}">
+                            <img class="img-circle" src="{{ $faker->imageUrl(250, 250, 'cats') }}">
                         </div>
 
                         @if( Auth::check())
@@ -62,20 +62,24 @@
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <h3 class="page-header">Discogs Database</h3>
-                        <p>This copy of {{ $discogs_record->title }} is a {{ !empty($discogs_record->styles[0]) ? $discogs_record->styles[0] : '' }}
-                            {{ $discogs_record->formats[0]->descriptions[0] }} {{ !empty($discogs_record->formats[0]->descriptions[1]) ? $discogs_record->formats[0]->descriptions[1] : '' }},
+                        <p>This copy of {{ $discogs_record->title }} is a {{ isset($discogs_record->styles[0]) ? $discogs_record->styles[0] : '' }}
+                            {{ $discogs_record->formats[0]->descriptions[0] }} {{ isset($discogs_record->formats[0]->descriptions[1]) ? $discogs_record->formats[0]->descriptions[1] : '' }},
                             pressed by {{ $discogs_record->labels[0]->name }}.
                         </p>
                         <p>The catalog number is: {{ $discogs_record->labels[0]->catno }}</p>
 
-                        @if( ! empty($discogs_record->notes) )
-                            <p>{{ $discogs_record->notes }}</p><br>
+                        @if( isset($discogs_record->notes) )
+                            <ul>
+                                <li class="list-unstyled">{{ $discogs_record->notes }}</li>
+                            </ul>
                         @endif
 
-                        <p>{{$discogs_record_rating->rating->count}} Discogs users give this record an average rating of {{$discogs_record_rating->rating->average}} of 5</p>
+                        <p><strong>{{$discogs_record_rating->rating->count}}</strong> Discogs users give this record the average rating
+                            <strong>{{$discogs_record_rating->rating->average}} out of 5</strong>
+                        </p>
 
                         <div class="img-responsive text-center">
-                            <img class="img-circle" src="{{ $faker->imageUrl(300, 300, 'cats') }}">
+                            <img class="img-circle" src="{{ $faker->imageUrl(250, 250, 'cats') }}">
                         </div>
                     </div>
                 </div>
@@ -85,20 +89,24 @@
             <h2><a href ="" title="">{{ $discogs_record->artists[0]->name }}</a></h2>
             <h1 class=page-header">{{ $discogs_record->title }}</h1>
 
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <p>This copy of {{ $discogs_record->title }} is a {{ !empty($discogs_record->styles[0]) ? $discogs_record->styles[0] : '' }}
-                            {{ $discogs_record->formats[0]->descriptions[0] }} {{ !empty($discogs_record->formats[0]->descriptions[1]) ? $discogs_record->formats[0]->descriptions[1] : '' }},
-                            pressed by {{ $discogs_record->labels[0]->name }}.
+                        <p>This copy of {{ $discogs_record->title }} is a {{ isset($discogs_record->styles[0]) ? $discogs_record->styles[0] : '' }}
+                            {{ $discogs_record->formats[0]->descriptions[0] }} {{ isset($discogs_record->formats[0]->descriptions[1]) ? $discogs_record->formats[0]->descriptions[1] : '' }},
+                            pressed by {{ $discogs_record->labels[0]->name }}
                         </p>
                         <p>The catalog number is: {{ $discogs_record->labels[0]->catno }}</p>
 
-                        @if( ! empty($discogs_record->notes) )
-                            <p>{{ $discogs_record->notes }}.</p><br>
+                        @if( isset($discogs_record->notes) )
+                            <ul>
+                                <li class="list-unstyled">{{ $discogs_record->notes }}</li>
+                            </ul>
                         @endif
 
-                        <p>{{$discogs_record_rating->rating->count}} Discogs users give this record an average rating of {{$discogs_record_rating->rating->average}} of 5</p>
+                        <p><strong>{{$discogs_record_rating->rating->count}}</strong> Discogs users give this record the average rating
+                            <strong>{{$discogs_record_rating->rating->average}} out of 5</strong>
+                        </p>
 
                         <div class="text-center img-responsive">
                             <img class="img-circle" src="{{ $faker->imageUrl(250, 250, 'cats') }}">
@@ -110,7 +118,7 @@
                     {!! Form::open(['url' => '/record/store-from-discogs']) !!}
                         <input type="hidden" name="artist-name" value="{{ $discogs_record->artists[0]->name }}">
                         <input type="hidden" name="title" value="{{ $discogs_record->title }}">
-                        <input type="hidden" name="genre-name" value="{{ $discogs_record->styles[0] }}">
+                        <input type="hidden" name="genre-name" value="{{ isset($discogs_record->styles[0]) ? $discogs_record->styles[0] : '' }}">
                         <input type="hidden" name="label-name" value="{{ $discogs_record->labels[0]->name }}">
                         <input type="hidden" name="discogs-id" value="{{ $discogs_record->id }}">
                         <input type="hidden" name="catalog-number" value="{{ $discogs_record->labels[0]->catno }}">
@@ -124,12 +132,7 @@
                 @endif
             </div>
         @endif
-    </div>
-
-    <div class="row">
-        <div class ="container">
-            <br><br><a href="/record" class="btn btn-default btn-lg" role="button">Back to all records</a>
-        </div>
+        <br><br><p><a href="/record" class="btn btn-default btn-lg" role="button">Back to all records</a></p>
     </div>
 
 @endsection
